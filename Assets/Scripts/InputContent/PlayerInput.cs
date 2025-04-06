c3d3e4a12ed6b5ff8b4a882a651acedc5199b538
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-04-06 13:20:26 +0400

    AddMoveSystem

--- Assets/Scripts/InputContent/PlayerInput.cs
+++ Assets/Scripts/InputContent/PlayerInput.cs
@@ -5,12 +5,25 @@ namespace InputContent
 {
     public class PlayerInput : MonoBehaviour
     {
+        private const string MouseX = "Mouse X";
+        private const string MouseY = "Mouse Y";
+
         [SerializeField] private PlayerMovement _playerMovement;
+        [SerializeField] private LookAround _lookAround;
         [SerializeField] private Joystick _joystick;
+
+        private bool _isRotating;
 
         private void Update()
         {
-            _playerMovement.Move(_joystick.Horizontal,_joystick.Vertical);
+            if (_isRotating)
+                _lookAround.Looking(Input.GetAxis(MouseX), Input.GetAxis(MouseY));
+
+            _playerMovement.Move(_joystick.Horizontal, _joystick.Vertical);
         }
+
+        public void OnPointerDown()=> _isRotating = true;
+
+        public void OnPointerUp()=> _isRotating = false;
     }
 }
--- Assets/Scripts/PlayerContent/LookAround.cs
+++ Assets/Scripts/PlayerContent/LookAround.cs
@@ -1,53 +1,26 @@
-using System;
+using InputContent;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class LookAround : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+namespace PlayerContent
 {
-    public Transform playerBody; // Ссылка на тело персонажа
-    public float lookSpeed = 2f; // Скорость вращения камеры
-    public float verticalLookLimit = 80f; // Лимит вращения камеры по вертикали
-
-    private float rotationX = 0;
-    private bool isRotating = false;
-
-    private void Update()
+    public class LookAround : MonoBehaviour
     {
-        if (isRotating)
+        [SerializeField] private PlayerInput _playerInput;
+
+        public Transform playerBody;
+        public float lookSpeed = 2f;
+
+        private float _verticalLookLimit = 80f;
+        private float rotationX = 0;
+        private bool isRotating = false;
+
+        public void Looking(float x, float y)
         {
-            // Вращение камеры по вертикали и горизонтали
-            rotationX -= Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -verticalLookLimit, verticalLookLimit);
-
-            float rotationY = Input.GetAxis("Mouse X") * lookSpeed;
-
-            // Применение вращения камеры
+            rotationX -= y * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -_verticalLookLimit, _verticalLookLimit);
+            float rotationY = x * lookSpeed;
             transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             playerBody.Rotate(Vector3.up * rotationY);
         }
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerDown");
-        isRotating = true;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerUp");
-        isRotating = false;
-    }
-    
-    public void StartRotation()
-    {
-        isRotating = true;
-        Debug.Log("StartRotation called");
-    }
-
-    public void StopRotation()
-    {
-        isRotating = false;
-        Debug.Log("StopRotation called");
     }
 }
