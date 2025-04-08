78d66278fd8fa73da77f0bb8e07374ed906a9c80
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-04-08 16:50:45 +0400

    AddScrollProducts

---
+++ Assets/Scripts/UI/Screens/ShopContent/ShopPages/PageContents/WorksPage/MenuScrollContent.cs
@@ -0,0 +1,10 @@
+namespace UI.Screens.ShopContent.ShopPages.PageContents.WorksPage
+{
+    public class MenuScrollContent : PageScrollContent
+    {
+        public override void Init()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Screens/ShopContent/ShopPages/PageContents/WorksPage/WorkersScrollContent.cs
@@ -0,0 +1,9 @@
+namespace UI.Screens.ShopContent.ShopPages.PageContents.WorksPage
+{
+    public class WorkersScrollContent : PageScrollContent
+    {
+        public override void Init()
+        {
+        }
+    }
+}
