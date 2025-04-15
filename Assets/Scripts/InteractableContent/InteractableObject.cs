4845cb027f44aecd7903224188051ff2705136ec
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-04-15 14:24:37 +0400

    AddhighlightAsset

--- Assets/Scripts/InteractableContent/InteractableObject.cs
+++ Assets/Scripts/InteractableContent/InteractableObject.cs
@@ -1,26 +1,38 @@
 using System;
+using HighlightPlus;
 using Interfaces;
 using PlayerContent;
 using UnityEngine;
 
 namespace InteractableContent
 {
-    public class InteractableObject : MonoBehaviour,IInteractable
+    public class InteractableObject : MonoBehaviour, IInteractable
     {
         [SerializeField] private Outline _outline;
-        
+        [SerializeField] private HighlightEffect[] _highlightEffects;
+
         public event Action<PlayerInteraction> OnAction;
-    
+
         public void EnableOutline()
         {
             // _outline.enabled = true;
-            _outline.OutlineWidth = 10;
+            if (_highlightEffects.Length > 0)
+                foreach (var effect in _highlightEffects)
+                    effect.enabled = true;
+
+            if (_outline != null)
+                _outline.OutlineWidth = 10;
         }
 
         public void DisableOutline()
         {
             // _outline.enabled = false;
-            _outline.OutlineWidth = 0;
+            if (_outline != null)
+                _outline.OutlineWidth = 0;
+
+            if (_highlightEffects.Length > 0)
+                foreach (var effect in _highlightEffects)
+                    effect.enabled = false;
         }
 
         public virtual void Action(PlayerInteraction playerInteraction)
