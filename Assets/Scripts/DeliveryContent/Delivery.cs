fcaf1d77fab7d72e37c3d6861980338448eaee3a
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-05-01 14:47:20 +0400

    Add

--- Assets/Scripts/DeliveryContent/Delivery.cs
+++ Assets/Scripts/DeliveryContent/Delivery.cs
@@ -24,41 +24,11 @@ namespace DeliveryContent
 
         private void Start()
         {
-            // Загружаем сохраненные данные при старте
             LoadDeliveryData();
-            // Проверяем пропущенные доставки из-за отсутствия игрока
-
-            // CheckMissedDeliveries();
 
             if (_items.Count > 0 && !_isSpawning)
-            {
                 SpawnItems();
-            }
         }
-
-        private void OnApplicationQuit()
-        {
-            // Сохраняем время выхода
-            // SaveLastExitTime();
-        }
-
-        /*
-        private void OnApplicationPause(bool pauseStatus)
-        {
-            if (pauseStatus)
-            {
-                Debug.Log("pauseStatus " + pauseStatus);
-                // Сохраняем время выхода при паузе (для мобильных устройств)
-                SaveLastExitTime();
-            }
-            else
-            {
-                Debug.Log("pauseStatus " + pauseStatus);
-                // Проверяем пропущенные доставки при возвращении
-                CheckMissedDeliveries();
-            }
-        }
-        */
 
         public void AddItemsCart(List<ItemCart> items)
         {
@@ -130,13 +100,7 @@ namespace DeliveryContent
 
             Debug.Log($"Общее количество доставок: {_amountDeliveries}");
         }
-
-        /*private void SaveLastExitTime()
-        {
-            PlayerPrefs.SetString(LastExitTimeKey, DateTime.UtcNow.ToString("O"));
-            PlayerPrefs.Save();
-        }*/
-
+        
         private DeliverySaveWrapper ConvertToSaveData()
         {
             var wrapper = new DeliverySaveWrapper
@@ -151,8 +115,7 @@ namespace DeliveryContent
 
             return wrapper;
         }
-
-        // 5. Конвертация после загрузки
+        
         private List<ItemDeliveryInfo> ConvertFromSaveData(DeliverySaveWrapper wrapper)
         {
             var result = new List<ItemDeliveryInfo>();
@@ -252,79 +215,5 @@ namespace DeliveryContent
                 Amount = amount;
             }
         }
-
-/*private void SaveItems()
-{
-    string itemsJson = JsonUtility.ToJson(_items);
-    PlayerPrefs.SetString("SavedDeliveryItems", itemsJson);
-    PlayerPrefs.Save();
-}
-
-private void LoadSavedData()
-{
-    // Загружаем сохраненные предметы
-    string savedItems = PlayerPrefs.GetString("SavedDeliveryItems", "");
-
-    if (!string.IsNullOrEmpty(savedItems))
-    {
-        _items = JsonUtility.FromJson<List<ItemDeliveryInfo>>(savedItems);
-    }
-
-    // Загружаем время последнего выхода
-    string lastExitTimeString = PlayerPrefs.GetString(LastExitTimeKey, "");
-
-    if (!string.IsNullOrEmpty(lastExitTimeString))
-    {
-        _lastExitTime = DateTime.Parse(lastExitTimeString, null,
-            System.Globalization.DateTimeStyles.RoundtripKind);
-    }
-}*/
-
-/*private void CheckMissedDeliveries()
-{
-    if (!PlayerPrefs.HasKey(LastExitTimeKey)) return;
-
-    DateTime currentTime = DateTime.UtcNow;
-    TimeSpan absenceTime = currentTime - _lastExitTime;
-
-    // Вычисляем сколько доставок пропущено
-    float totalSeconds = (float)absenceTime.TotalSeconds;
-    int missedDeliveries = Mathf.FloorToInt(totalSeconds / _deliveryConfig.MinValueTimer);
-
-    Debug.Log($"Игрок отсутствовал {absenceTime.TotalMinutes} минут. Пропущено доставок: {missedDeliveries}");
-
-    // Обрабатываем пропущенные доставки
-    ProcessMissedDeliveries(missedDeliveries);
-
-    // Обновляем количество
-    UpdateAmountDeliveries();
-
-    // Сохраняем изменения
-    SaveItems();
-}*/
-
-/*private void ProcessMissedDeliveries(int missedCount)
-{
-    while (missedCount > 0 && _items.Count > 0)
-    {
-        ItemDeliveryInfo currentItem = _items[0];
-        currentItem.Amount--;
-        missedCount--;
-
-        if (currentItem.Amount <= 0)
-        {
-            _items.RemoveAt(0);
-            Debug.Log($"Автоматически завершена доставка: {currentItem.ItemType}");
-        }
-    }
-
-    if (missedCount > 0)
-    {
-        Debug.Log($"Всего пропущено доставок: {missedCount} (включая уже завершённые)");
-    }
-
-    UpdateAmountDisplay();
-    SaveGameData();
-}*/
     }
 }
