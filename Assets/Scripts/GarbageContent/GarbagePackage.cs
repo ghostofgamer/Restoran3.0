873a311810406c81ebf3b0a865598c77360dc5d8
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-05-07 14:32:32 +0400

    AddNewOwnedColor

--- Assets/Scripts/GarbageContent/GarbagePackage.cs
+++ Assets/Scripts/GarbageContent/GarbagePackage.cs
@@ -1,9 +1,26 @@
+using InteractableContent;
+using PlayerContent;
 using UnityEngine;
 
 namespace GarbageContent
 {
     public class GarbagePackage : MonoBehaviour
     {
-        // [SerializeField]private 
+        [SerializeField] private InteractableObject _interactableObject;
+
+        private void OnEnable()
+        {
+            _interactableObject.OnAction += Action;
+        }
+
+        private void OnDisable()
+        {
+            _interactableObject.OnAction -= Action;
+        }
+
+        private void Action(PlayerInteraction playerInteraction)
+        {
+            
+        }
     }
 }
