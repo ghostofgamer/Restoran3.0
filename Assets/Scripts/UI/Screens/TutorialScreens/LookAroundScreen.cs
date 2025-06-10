ec6f1401a0b60f444ea54d6cd276032343700859
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-06-10 15:05:37 +0400

    AddLocalizTutorFirst

--- Assets/Scripts/UI/Screens/TutorialScreens/LookAroundScreen.cs
+++ Assets/Scripts/UI/Screens/TutorialScreens/LookAroundScreen.cs
@@ -1,3 +1,5 @@
+using I2.Loc;
+using SettingsContent;
 using TMPro;
 using UnityEngine;
 
@@ -6,10 +8,29 @@ namespace UI.Screens.TutorialScreens
     public class LookAroundScreen : AbstractScreen
     {
         [SerializeField] private TMP_Text _title;
+        [SerializeField] private LanguageChanger _languageChanger;
+        
+        private void OnEnable()
+        {
+            _languageChanger.LanguageChanged += LanguageChange;
+        }
 
+        private void OnDisable()
+        {
+            _languageChanger.LanguageChanged -= LanguageChange;
+        }
+        
+        
         private void Start()
         {
-            _title.text = "Use <color=yellow>right thumb</color> to look around";
+            // _title.text = "Use <color=yellow>right thumb</color> to look around";
+            LanguageChange();
+        }
+        
+        private void  LanguageChange()
+        {
+            _title.text =
+                $"{LocalizationManager.GetTermTranslation("Use")} <color=yellow>{LocalizationManager.GetTermTranslation("right thumb")}</color> {LocalizationManager.GetTermTranslation("look around")}";
         }
     }
 }
