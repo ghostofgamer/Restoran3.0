b0c58dafe3bc39b9dd513384c039d69bcbb68b12
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-06-25 19:21:52 +0400

    AddWheelRotate

--- Assets/Scripts/CityTrafficContent/AbstractNPC.cs
+++ Assets/Scripts/CityTrafficContent/AbstractNPC.cs
@@ -9,7 +9,7 @@ namespace CityTrafficContent
         [SerializeField] private Animator _animator;
         [SerializeField] private float _minDistance;
 
-        private ICityTraffic  _npcTraffic;
+        private ICityTraffic _npcTraffic;
         private Transform[] _points;
         private int _index = 0;
 
@@ -19,25 +19,16 @@ namespace CityTrafficContent
         }
 
         public abstract void InitUniqueData();
-        
+
         public void Init(GameObject path, ICityTraffic npcTraffic)
         {
+            InitUniqueData();
             _npcTraffic = npcTraffic;
             _points = new Transform[path.transform.childCount];
+
             for (int i = 0; i < _points.Length; i++)
                 _points[i] = path.transform.GetChild(i);
         }
-        
-        /*public void Init<T>(GameObject path, CityTraffic<T> npcTraffic) where T : AbstractNPC
-        {
-            Debug.Log("npcTraffic " + npcTraffic);
-            _npcTraffic = npcTraffic as CityTraffic<AbstractNPC>;
-            _points = new Transform[path.transform.childCount];
-            for (int i = 0; i < _points.Length; i++)
-                _points[i] = path.transform.GetChild(i);
-            /*ChoiceAccessories();
-            ChoiceAppearance();#1#
-        }*/
 
         private void Roam()
         {
@@ -54,35 +45,9 @@ namespace CityTrafficContent
             }
 
             _navMeshAgent.SetDestination(_points[_index].position);
-            _animator.SetFloat("Vertical", !_navMeshAgent.isStopped ? 1 : 0);
+            
+            if (_animator != null)
+                _animator.SetFloat("Vertical", !_navMeshAgent.isStopped ? 1 : 0);
         }
-
-        /*private void ChoiceAccessories()
-        {
-            foreach (var accessory in _accessories)
-                accessory.SetActive(false);
-
-            _accessories[Random.Range(0, _accessories.Length)].SetActive(true);
-        }
-
-        private void ChoiceAppearance()
-        {
-            foreach (var body in _bodies)
-                body.SetActive(false);
-
-            foreach (var eyes in _eyes)
-                eyes.SetActive(false);
-
-            foreach (var tail in _tails)
-                tail.SetActive(false);
-
-            foreach (var mounth in _mounth)
-                mounth.SetActive(false);
-
-            _bodies[Random.Range(0, _bodies.Length)].SetActive(true);
-            _eyes[Random.Range(0, _eyes.Length)].SetActive(true);
-            _tails[Random.Range(0, _tails.Length)].SetActive(true);
-            _mounth[Random.Range(0, _mounth.Length)].SetActive(true);
-        }*/
     }
 }
