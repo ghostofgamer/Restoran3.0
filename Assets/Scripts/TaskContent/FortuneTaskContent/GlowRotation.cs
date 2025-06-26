7a472241da743b42362c817bea6502324601b674
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-06-26 17:07:04 +0400

    AddNewLevelUIBase

--- Assets/Scripts/TaskContent/FortuneTaskContent/GlowRotation.cs
+++ Assets/Scripts/TaskContent/FortuneTaskContent/GlowRotation.cs
@@ -4,9 +4,11 @@ namespace TaskContent.FortuneTaskContent
 {
     public class GlowRotation : MonoBehaviour
     {
+        [SerializeField] private float _speed = 5f;
+        
         private void Update()
         {
-            transform.Rotate(Vector3.forward, 5 * Time.deltaTime);
+            transform.Rotate(Vector3.forward, _speed * Time.deltaTime);
         }
     }
 }
