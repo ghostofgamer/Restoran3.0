eafe977faf39aeda88fcd19b5a3fee81c6d8596e
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-06-26 10:53:50 +0400

    AddNewCars

--- Assets/Scripts/CityTrafficContent/CarNPC.cs
+++ Assets/Scripts/CityTrafficContent/CarNPC.cs
@@ -4,9 +4,19 @@ namespace CityTrafficContent
 {
     public class CarNPC : AbstractNPC
     {
+        [SerializeField] private Texture[] _textures;
+        [SerializeField] private Renderer _renderer;
+
+        private Material _material;
+        
         public override void InitUniqueData()
         {
-            
+            if (_renderer != null)
+            {
+                _material = new Material(_renderer.material);
+                _material.mainTexture = _textures[Random.Range(0, _textures.Length)];
+                _renderer.material = _material;
+            }
         }
     }
 }
