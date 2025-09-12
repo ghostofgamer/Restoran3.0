b73e93cebb419ebed020398e97242a4e92b71246
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-09-12 12:35:36 +0400

    AddStylePageShop

---
+++ Assets/Scripts/UI/Screens/ShopContent/ShopPages/PageContents/StylePage/DecorScrollContent.cs
@@ -0,0 +1,9 @@
+namespace UI.Screens.ShopContent.ShopPages.PageContents.StylePage
+{
+    public class DecorScrollContent : PageScrollContent
+    {
+        public override void Init()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Screens/ShopContent/ShopPages/PageContents/StylePage/FurnitureScrollContent.cs
@@ -0,0 +1,10 @@
+namespace UI.Screens.ShopContent.ShopPages.PageContents.StylePage
+{
+    public class FurnitureScrollContent : PageScrollContent
+    {
+        public override void Init()
+        {
+        
+        }
+    }
+}
