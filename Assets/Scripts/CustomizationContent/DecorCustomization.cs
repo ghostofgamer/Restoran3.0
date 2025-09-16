417d399b4be29f521f04300b4264cebc527dcc07
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-09-16 18:10:03 +0400

    AddIconsPaintin

--- Assets/Scripts/CustomizationContent/DecorCustomization.cs
+++ Assets/Scripts/CustomizationContent/DecorCustomization.cs
@@ -4,35 +4,35 @@ namespace CustomizationContent
 {
     public class DecorCustomization : MonoBehaviour
     {
-        [SerializeField]private GameObject[]   _plants;
+        [SerializeField] private GameObject[] _plants;
         [SerializeField] private GameObject[] _paintings;
-        [SerializeField]private GameObject[] _stickers;
-        [SerializeField]private GameObject[] _shelves;
-        [SerializeField]private GameObject[] _others;
-        
+        [SerializeField] private GameObject[] _stickers;
+        [SerializeField] private GameObject[] _shelves;
+        [SerializeField] private GameObject[] _others;
+
         public void ChangeActivityPlants(int index)
         {
-            _plants[index].SetActive(true);
+            _plants[index].SetActive(!_plants[index].activeSelf);
         }
 
         public void ChangeActivityPaintings(int index)
         {
-            _paintings[index].SetActive(true);
+            _paintings[index].SetActive(!_paintings[index].activeSelf);
         }
 
         public void ChangeActivityStickers(int index)
         {
-            _stickers[index].SetActive(true);
+            _stickers[index].SetActive(!_stickers[index].activeSelf);
         }
 
         public void ChangeActivityShelves(int index)
         {
-            _shelves[index].SetActive(true);
+            _shelves[index].SetActive(!_shelves[index].activeSelf);
         }
 
         public void ChangeActivityOthers(int index)
         {
-            _others[index].SetActive(true);
+            _others[index].SetActive(!_others[index].activeSelf);
         }
     }
 }
